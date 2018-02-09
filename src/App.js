bd126aa9ee1d9057095a4cb63b7d2a9167b536ea
Nicklas Pouey-Winger <nicklas.pouey-winger@evry.com>
2018-02-09 14:04:45 +0100

    Enable HMR

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Heading';
+    const mockUserObj = {
+      name: 'foo',
+      lastName: 'bar'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{helloWorld}</h2>
+        <p>{mockUserObj.name}</p>
+        <p>{mockUserObj.lastName}</p>
       </div>
     );
   }
